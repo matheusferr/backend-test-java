c1fa084b0fe4a4b0af82c5683fbafe3ed660824d
Matheus Ferreira <math.ferr@outlook.com>
2021-08-05 13:49:20 -0300

    Updated service implementation

--- src/main/java/br/com/fcamara/teste/dev/service/implementation/EntradaSaidaServiceServiceImpl.java
+++ src/main/java/br/com/fcamara/teste/dev/service/implementation/EntradaSaidaServiceServiceImpl.java
@@ -8,6 +8,8 @@ import br.com.fcamara.teste.dev.repository.VagaRepository;
 import br.com.fcamara.teste.dev.service.definition.EntradaSaidaService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EntradaSaidaServiceServiceImpl implements EntradaSaidaService {
     private EstabelecimentoServiceImpl estabelecimentoServiceImpl;
@@ -21,13 +23,41 @@ public class EntradaSaidaServiceServiceImpl implements EntradaSaidaService {
         this.vagaRepository = vagaRepository;
     }
 
+    private boolean isPresent(List<Vaga> vagas, Veiculo veiculo) {
+        return vagas.stream().anyMatch(vaga -> vaga.getVeiculo().equals(veiculo));
+    }
+
     @Override
     public Vaga addVehicle(String placa, String cnpj) {
-        return null;
+        Veiculo veiculo = veiculoServiceImpl.findByPlaca(placa);
+        Estabelecimento estabelecimento = estabelecimentoServiceImpl.findByCnpj(cnpj);
+        List<Vaga> vagas = estabelecimento.getVagas();
+
+        if (this.isPresent(vagas, veiculo)) throw new OperacaoInvalidaException(
+                "veiculo já vinculado ao estacionamento"
+        );
+
+        Vaga vaga = new Vaga(veiculo);
+
+        vagas.add(vaga);
+
+        return vagaRepository.save(vaga);
     }
 
     @Override
     public void removeVehicle(String placa, String cnpj) {
+        Veiculo veiculo = veiculoServiceImpl.findByPlaca(placa);
+        Estabelecimento estabelecimento = estabelecimentoServiceImpl.findByCnpj(cnpj);
+        List<Vaga> vagas = estabelecimento.getVagas();
 
+        if (!this.isPresent(vagas, veiculo)) throw new OperacaoInvalidaException(
+                "veiculo não vinculado ao estacionamento"
+        );
+
+        Vaga vaga = new Vaga(veiculo);
+
+        vagas.remove(vaga);
+
+        vagaRepository.delete(vaga);
     }
 }
